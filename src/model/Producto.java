9dbb8410f6ec4a02ca11b7758a476de9ab51c973
jimmygomez2013422 <3115058800409@ingenieria.usac.edu.gt>
2024-09-18 23:31:30 -0600

    mi practica no.1

---
+++ src/model/Producto.java
@@ -0,0 +1,26 @@
+
+package model;
+
+public class Producto {
+    
+    //Atrivutos privados
+    private String codigo;
+    private String material;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    
+}
